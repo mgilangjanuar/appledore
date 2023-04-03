e19a7a6c9657383e5e22de9093d02513fe3394a1
M Gilang Januar <mgilangjanuar@gmail.com>
2023-04-03 11:03:11 +0700

    feat(vite-config): add markdown plugin with syntax highlighting
    Add `vite-plugin-markdown` and `highlight.js` to support rendering markdown files with syntax highlighting in the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import hljs from 'highlight.js'
+import { defineConfig } from 'vite'
+import { Mode, plugin as mdPlugin } from 'vite-plugin-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mdPlugin({
+    mode: [Mode.HTML],
+    markdownIt: {
+      typographer: true,
+      highlight: function (str: string, lang: string) {
+        try {
+          return `<pre class="hljs">${str
+            .split('\n')
+            .map(s => `${hljs.highlight(s, {
+              language: lang, ignoreIllegals: true
+            }).value}`)
+            .join('<br />')}</pre>`
+        } catch (error) {
+          console.error(error)
+        }
+
+        return `<pre class="hljs">${str}</pre>`
+      }
+    }
+  })],
 })
